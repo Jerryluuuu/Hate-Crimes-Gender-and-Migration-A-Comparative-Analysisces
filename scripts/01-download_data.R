0e38b7d5519facd4ef3636e3a02612cac60fb7ee
Jerryluuuu <Jerrye33692@gmail.com>
2024-02-08 23:15:05 +0000

    Download rawdata, and change the file type from .dta to .csv.

---
+++ scripts/01-download_data.R
@@ -0,0 +1,18 @@
+#### Preamble ####
+#### Preamble ####
+# Purpose: Simulating data on survery results 
+# Authors: Jerry Lu, YuChen Chen, Che-Yu Wang 
+# Email: Jerry33692@gmail.com, Victorchen.chen@mail.utoronto.ca  
+# License: MIT
+# Pre-requisites: Downloaded survey.dta from https://doi.org/10.7910/DVN/QXJDJ5
+
+####Workspace setup####
+install.packages("readstata13")
+library(readstata13)
+library(readr)
+
+#reading in the stata file
+survey <- read.dta13(here::here("inputs/data/survey.dta"))
+
+#writing to csv
+write_csv(survey, here::here("inputs/data/survey.csv"))
